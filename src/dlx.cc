e789c779b9c723188e1e3016245b4ef9b86a16cd
kunlun@nmb <likunlun@huwaijijie.com>
2018-09-11 23:23:36 +0800

    sync:

--- src/dlx.cc
+++ src/dlx.cc
@@ -4,6 +4,34 @@
 using namespace std;
 
 
+napi_value methodSolutionCount (napi_env env, napi_callback_info info) {
+    napi_status status;
+    size_t argc=1;
+    napi_value argv[1];
+    status = napi_get_cb_info(env, info, &argc, argv, 0, 0);
+    if( status != napi_ok) {
+        napi_throw_type_error(env, "111", "Wrong number of arguments");
+        return 0; // napi_value 实际上是一个指针，返回空指针表示无返回值
+    }
+    DLXSolver dlx;
+    size_t len;
+    char pszstr[100]="";
+    status = napi_get_value_string_latin1(env, argv[0], pszstr, 100, &len);
+    if( status != napi_ok) {
+        napi_throw_type_error(env, "112", "Can't read arguments");
+        return 0; // napi_value 实际上是一个指针，返回空指针表示无返回值
+    }
+    int cnt = dlx.solution_count(pszstr);
+
+    napi_value retcnt;
+    status = napi_create_int64(env, (size_t)cnt, &retcnt);
+    if( status != napi_ok) {
+        napi_throw_type_error(env, "113", "set cnt error");
+        return 0; // napi_value 实际上是一个指针，返回空指针表示无返回值
+    }
+    return retcnt;
+}
+
 napi_value methodSolve (napi_env env, napi_callback_info info) {
     napi_status status;
     size_t argc=1;
@@ -61,9 +89,11 @@ napi_value Init(napi_env env, napi_value exports) {
   napi_status status;
   // https://nodejs.org/docs/latest-v8.x/api/n-api.html#n_api_napi_property_descriptor
   napi_property_descriptor descSolve = { "solve", 0, methodSolve, 0, 0, 0, napi_default, 0 };
+  napi_property_descriptor descSolutionCount = { "solutioncount", 0, methodSolutionCount, 0, 0, 0, napi_default, 0 };
   napi_property_descriptor descGenerate = { "generate", 0, methodGenerate, 0, 0, 0, napi_default, 0 };
 
   status = napi_define_properties(env, exports, 1, &descSolve);
+  status = napi_define_properties(env, exports, 1, &descSolutionCount);
   status = napi_define_properties(env, exports, 1, &descGenerate);
   if (status != napi_ok) return NULL;
   return exports;
--- test.js
+++ test.js
@@ -12,3 +12,16 @@ for(let i=0;i<10;i++){
   const {puzzle, solution} = dlx.generate();
   console.log(`${puzzle} => ${solution}`);
 }
+
+// multi test:
+const pszmulti = '000000000000000000000000000000900000000000050008007010907000100000040000000350060';
+console.log(dlx.solve(pszmulti));
+console.log(dlx.solutioncount(pszmulti));
+
+const pszvalid = '000000000530000000000008702000900000000000050008007010907000100000040000000350060';
+console.log(dlx.solve(pszvalid));
+console.log(dlx.solutioncount(pszvalid));
+
+const psznotvalid = '110000000530000000000008702000900000000000050008007010907000100000040000000350060';
+console.log(dlx.solve(psznotvalid));
+console.log(dlx.solutioncount(psznotvalid));
